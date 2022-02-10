9b5771369567a5fbab1aff7a2500523402c3bf2f
EPAM\Leonardo_Yarto <leo.yarto@gmail.com>
2022-02-09 23:36:01 -0600

    Added sqrt

--- Main.java
+++ Main.java
@@ -13,6 +13,9 @@ class Calculator{
     public double mult(double a, double b){
         return a*b;
     }
+    public double sqrt(double a){
+        return java.lang.Math.sqrt (a);
+    }
 }
 
 public class Main{
