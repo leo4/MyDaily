59fa46cc356fef973c8e6fbdd6fd818ecc51ae9f
EPAM\Leonardo_Yarto <leo.yarto@gmail.com>
2022-02-09 23:20:55 -0600

    added subtract

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 class Calculator{
-    public int add(int a, int b){
+        public double add(double a, double b){
         return a+b;
+    }
+    public double subtract(double a, double b){
+        return a-b;
     }
 }
 
