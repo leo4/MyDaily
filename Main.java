47721d5252574f836b481758f1287a7e486dc8fc
EPAM\Leonardo_Yarto <leo.yarto@gmail.com>
2022-02-10 00:04:30 -0600

    added sum 4 params

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 class Calculator{
         public double add(double a, double b){
-            System.out.println("This is add method");
+            System.out.println("This is add 2 method");
         return a+b;
+    }
+    public double add(double a, double b,double c){
+        System.out.println("This is add method 3 params");
+        return a+b+c;
+    }
+    public double add(double a, double b,double c,double d){
+        System.out.println("This is add method 4 params");
+        return a+b+c+d;
     }
     public double subtract(double a, double b){
         return a-b;
