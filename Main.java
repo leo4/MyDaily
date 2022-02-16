e653b366c28964c85e043b147973f26e523894c0
EPAM\Leonardo_Yarto <leo.yarto@gmail.com>
2022-02-16 17:03:45 -0600

    Added BigInt cherry picked
    (cherry picked from commit 7880c780e907b61b1676c40d0ece4df9c33d9a51)

--- Main.java
+++ Main.java
@@ -1,21 +1,39 @@
+import java.math.BigInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 class Calculator{
+
         public double add(double a, double b){
 
         public BigInteger add(BigInteger a, BigInteger b){
+
+        private final static Logger LOGGER = Logger.getLogger("bitacora.subnivel.Control");
+        public BigInteger add(BigInteger  a, BigInteger  b){
+
             System.out.println("This is add 2 method");
-        return a+b;
+
+                BigInteger result =a.add(b);
+            LOGGER.log(Level.INFO, "added succesfully 2 params :"+a+"+"+b+" Result is:"+result);
+
+            return result;
     }
-     public BigInteger add(BigInteger a, BigInteger b){
-            System.out.println("This is add 2 method");
-        return a+b;
-    }
-    public double add(BigInteger a, BigInteger b,BigInteger c){
+
+    public BigInteger add(BigInteger a, BigInteger b,BigInteger c){
         System.out.println("This is add method 3 params");
-        return a+b+c;
+
+        BigInteger result =a.add(b).add(c);
+        LOGGER.log(Level.INFO, "added succesfully 3 params :"+a+"+"+b+"+"+c+" Result is:"+result);
+
+        return result;
+
     }
-    public double add(BigInteger a, BigInteger b,BigInteger c,BigInteger d){
+    public BigInteger add(BigInteger a, BigInteger b,BigInteger c,BigInteger d){
         System.out.println("This is add method 4 params");
-        return a+b+c+d;
+        BigInteger result =a.add(b).add(c).add(d);
+        LOGGER.log(Level.INFO, "added succesfully 4 params :"+a+"+"+b+"+"+c+"+"+d+" Result is:"+result);
+
+        return result;
     }
     public double subtract(double a, double b){
         return a-b;
@@ -35,6 +53,8 @@ class Calculator{
 
 public class Main{
     public static void main(String[] args){
-
+        Calculator calc = new Calculator();
+ calc.add(BigInteger.valueOf(2),BigInteger.valueOf(3));
+        calc.add(BigInteger.valueOf(2),BigInteger.valueOf(3),BigInteger.valueOf(4));
         }
         }
